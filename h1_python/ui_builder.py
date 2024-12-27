7df5dcbb8b9ed9722d03a61fc0c3950ffa2635d4
srobots <katsuhiro.kotaki@keio.jp>
2024-12-27 21:40:34 +0900

    Anabled Motion

--- h1_python/ui_builder.py
+++ h1_python/ui_builder.py
@@ -1,12 +1,3 @@
-# Copyright (c) 2022-2024, NVIDIA CORPORATION. All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto. Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-#
-
 import numpy as np
 import omni.timeline
 import omni.ui as ui
@@ -24,18 +15,11 @@ from pxr import Sdf, UsdLux
 
 from .scenario import ExampleScenario
 
-
 class UIBuilder:
     def __init__(self):
-        # Frames are sub-windows that can contain multiple UI elements
         self.frames = []
-        # UI elements created using a UIElementWrapper instance
         self.wrapped_ui_elements = []
-
-        # Get access to the timeline to control stop/pause/play programmatically
         self._timeline = omni.timeline.get_timeline_interface()
-
-        # Run initialization for the provided example
         self._on_init()
 
     ###################################################################################
@@ -43,58 +27,25 @@ class UIBuilder:
     ###################################################################################
 
     def on_menu_callback(self):
-        """Callback for when the UI is opened from the toolbar.
-        This is called directly after build_ui().
-        """
         pass
 
     def on_timeline_event(self, event):
-        """Callback for Timeline events (Play, Pause, Stop)
-
-        Args:
-            event (omni.timeline.TimelineEventType): Event Type
-        """
         if event.type == int(omni.timeline.TimelineEventType.STOP):
-            # When the user hits the stop button through the UI, they will inevitably discover edge cases where things break
-            # For complete robustness, the user should resolve those edge cases here
-            # In general, for extensions based off this template, there is no value to having the user click the play/stop
-            # button instead of using the Load/Reset/Run buttons provided.
             self._scenario_state_btn.reset()
             self._scenario_state_btn.enabled = False
 
     def on_physics_step(self, step: float):
-        """Callback for Physics Step.
-        Physics steps only occur when the timeline is playing
-
-        Args:
-            step (float): Size of physics step
-        """
         pass
 
     def on_stage_event(self, event):
-        """Callback for Stage Events
-
-        Args:
-            event (omni.usd.StageEventType): Event Type
-        """
         if event.type == int(StageEventType.OPENED):
-            # If the user opens a new stage, the extension should completely reset
             self._reset_extension()
 
     def cleanup(self):
-        """
-        Called when the stage is closed or the extension is hot reloaded.
-        Perform any necessary cleanup such as removing active callback functions
-        Buttons imported from omni.isaac.ui.element_wrappers implement a cleanup function that should be called
-        """
         for ui_elem in self.wrapped_ui_elements:
             ui_elem.cleanup()
 
     def build_ui(self):
-        """
-        Build a custom UI tool to run your extension.
-        This function will be called any time the UI window is closed and reopened.
-        """
         world_controls_frame = CollapsableFrame("World Controls", collapsed=False)
 
         with world_controls_frame:
@@ -132,38 +83,18 @@ class UIBuilder:
 
     def _on_init(self):
         self._articulation = None
+        self._object_prim = None
         self._scenario = ExampleScenario()
 
     def _add_light_to_stage(self):
-        """
-        A new stage does not have a light by default.  This function creates a spherical light
-        """
         sphereLight = UsdLux.SphereLight.Define(get_current_stage(), Sdf.Path("/World/SphereLight"))
         sphereLight.CreateRadiusAttr(2)
         sphereLight.CreateIntensityAttr(100000)
         XFormPrim(str(sphereLight.GetPath())).set_world_pose([6.5, 0, 12])
 
     def _setup_scene(self):
-        """
-        This function is attached to the Load Button as the setup_scene_fn callback.
-        On pressing the Load Button, a new instance of World() is created and then this function is called.
-        The user should now load their assets onto the stage and add them to the World Scene.
-
-        In this example, a new stage is loaded explicitly, and all assets are reloaded.
-        If the user is relying on hot-reloading and does not want to reload assets every time,
-        they may perform a check here to see if their desired assets are already on the stage,
-        and avoid loading anything if they are.  In this case, the user would still need to add
-        their assets to the World (which has low overhead).  See commented code section in this function.
-        """
         robot_prim_path = "/h1"
         path_to_robot_usd = get_assets_root_path() + "/Isaac/Robots/Unitree/H1/h1.usd"
-
-        # Do not reload assets when hot reloading.  This should only be done while extension is under development.
-        # if not is_prim_path_valid(robot_prim_path):
-        #     create_new_stage()
-        #     add_reference_to_stage(path_to_robot_usd, robot_prim_path)
-        # else:
-        #     print("Robot already on Stage")
 
         create_new_stage()
         self._add_light_to_stage()
@@ -171,92 +102,47 @@ class UIBuilder:
         world = World(stage_units_in_meters=0.01)
         world.scene.add_default_ground_plane()
 
-        # Move robot along Z-axis
+        # Position the robot
         robot_prim = XFormPrim(robot_prim_path)
         robot_prim.set_world_pose([0.0, 0.0, 1.3])
 
         self._articulation = Articulation(robot_prim_path)
 
-        # Add user-loaded objects to the World
+        # Add an object for the scenario
+        object_prim_path = "/World/Object"
+        self._object_prim = XFormPrim(object_prim_path)
+        self._object_prim.set_world_pose([1.0, 1.0, 0.5])  # Initial position
+
+        # Add objects to the world
         world = World.instance()
         world.scene.add(self._articulation)
+        world.scene.add(self._object_prim)
 
     def _setup_scenario(self):
-        """
-        This function is attached to the Load Button as the setup_post_load_fn callback.
-        The user may assume that their assets have been loaded by their setup_scene_fn callback, that
-        their objects are properly initialized, and that the timeline is paused on timestep 0.
-
-        In this example, a scenario is initialized which will move each robot joint one at a time in a loop while moving the
-        provided prim in a circle around the robot.
-        """
         self._reset_scenario()
-
-        # UI management
         self._scenario_state_btn.reset()
         self._scenario_state_btn.enabled = True
         self._reset_btn.enabled = True
 
     def _reset_scenario(self):
         self._scenario.teardown_scenario()
-        self._scenario.setup_scenario(self._articulation)
+        self._scenario.setup_scenario(self._articulation, self._object_prim)
 
     def _on_post_reset_btn(self):
-        """
-        This function is attached to the Reset Button as the post_reset_fn callback.
-        The user may assume that their objects are properly initialized, and that the timeline is paused on timestep 0.
-
-        They may also assume that objects that were added to the World.Scene have been moved to their default positions.
-        I.e. the cube prim will move back to the position it was in when it was created in self._setup_scene().
-        """
         self._reset_scenario()
-
-        # UI management
         self._scenario_state_btn.reset()
         self._scenario_state_btn.enabled = True
 
     def _update_scenario(self, step: float):
-        """This function is attached to the Run Scenario StateButton.
-        This function was passed in as the physics_callback_fn argument.
-        This means that when the a_text "RUN" is pressed, a subscription is made to call this function on every physics step.
-        When the b_text "STOP" is pressed, the physics callback is removed.
-
-        Args:
-            step (float): The dt of the current physics step
-        """
         self._scenario.update_scenario(step)
 
     def _on_run_scenario_a_text(self):
-        """
-        This function is attached to the Run Scenario StateButton.
-        This function was passed in as the on_a_click_fn argument.
-        It is called when the StateButton is clicked while saying a_text "RUN".
-
-        This function simply plays the timeline, which means that physics steps will start happening.  After the world is loaded or reset,
-        the timeline is paused, which means that no physics steps will occur until the user makes it play either programmatically or
-        through the left-hand UI toolbar.
-        """
         self._timeline.play()
 
     def _on_run_scenario_b_text(self):
-        """
-        This function is attached to the Run Scenario StateButton.
-        This function was passed in as the on_b_click_fn argument.
-        It is called when the StateButton is clicked while saying a_text "STOP"
-
-        Pausing the timeline on b_text is not strictly necessary for this example to run.
-        Clicking "STOP" will cancel the physics subscription that updates the scenario, which means that
-        the robot will stop getting new commands and the cube will stop updating without needing to
-        pause at all.  The reason that the timeline is paused here is to prevent the robot being carried
-        forward by momentum for a few frames after the physics subscription is canceled.  Pausing here makes
-        this example prettier, but if curious, the user should observe what happens when this line is removed.
-        """
         self._timeline.pause()
 
     def _reset_extension(self):
-        """This is called when the user opens a new stage from self.on_stage_event().
-        All state should be reset.
-        """
         self._on_init()
         self._reset_ui()
 
